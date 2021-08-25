7187eedde85dcfde343cdfcf3ed8e90ccb41df28
Thiago Passarin <thiagopas@gmail.com>
2021-08-24 21:44:41 -0300

    Passo constante

--- passarin/simulador.py
+++ passarin/simulador.py
@@ -6,128 +6,161 @@ import matplotlib.animation
 from scipy import signal
 import time
 
-
-def laplaciano2(matriz):
+def laplaciano4(matriz):
+    #f_xx = (-1*f[i-2]+16*f[i-1]-30*f[i+0]+16*f[i+1]-1*f[i+2])/(12*1.0*h**2)
     M = matriz.shape[0]
     N = matriz.shape[1]
-    lap = np.zeros((M+2, N+2))
-    lap[1:-1, 1:-1] = -4 * matriz
+    lap = np.zeros((M+4, N+4))
+    lap[2:-2, 2:-2] = -60 * matriz
 
-    lap[0:-2, 1:-1] += matriz
-    lap[2:, 1:-1] += matriz
+    lap[0:-4, 2:-2] += -1 * matriz
+    lap[1:-3, 2:-2] += 16 * matriz
+    lap[3:-1, 2:-2] += 16 * matriz
+    lap[4:, 2:-2] += -1 * matriz
 
-    lap[1:-1, 2:] += matriz
-    lap[1:-1, 0:-2] += matriz
+    lap[2:-2, 0:-4] += -1 * matriz
+    lap[2:-2, 1:-3] += 16 * matriz
+    lap[2:-2, 3:-1] += 16 * matriz
+    lap[2:-2, 4:] += -1 * matriz
 
-    y = lap[1:-1, 1:-1]
-    return y
+    y = lap[2:-2, 2:-2]
+    return y/12
+
+def lap4(matriz):
+    #f_xx = (-1*f[i-2]+16*f[i-1]-30*f[i+0]+16*f[i+1]-1*f[i+2])/(12*1.0*h**2)
+    M = matriz.shape[0]
+    N = matriz.shape[1]
+    if matriz.shape.__len__() < 3:
+        T = 1
+    else:
+        T = matriz.shape[2]
+    lap = np.zeros((M + 4, N + 4, T))
+    lap[2:-2, 2:-2, :] = -60 * matriz.reshape((M, N, T))
+
+    lap[0:-4, 2:-2, :] += -1 * matriz.reshape((M, N, T))
+    lap[1:-3, 2:-2, :] += 16 * matriz.reshape((M, N, T))
+    lap[3:-1, 2:-2, :] += 16 * matriz.reshape((M, N, T))
+    lap[4:, 2:-2, :] += -1 * matriz.reshape((M, N, T))
+
+    lap[2:-2, 0:-4, :] += -1 * matriz.reshape((M, N, T))
+    lap[2:-2, 1:-3, :] += 16 * matriz.reshape((M, N, T))
+    lap[2:-2, 3:-1, :] += 16 * matriz.reshape((M, N, T))
+    lap[2:-2, 4:, :] += -1 * matriz.reshape((M, N, T))
+
+    y = lap[2:-2, 2:-2, :]
+    return y/12
 
 
+def simulacao(meio, termo_fonte=None, h=200e-6, ts=10e-9):
+    # Dados da simulação
+    #cmax = 5800  # Velocidade máxima no meio (aço)
+    #cmax = 1500  # Velocidade máxima no meio (água do mar)
 
-def gera_video_otimizacao(nome, nit):
-    metadata = dict(title='Simulação Ultrassom', author='Daniel Rossato')
-    writer = matplotlib.animation.FFMpegWriter(fps=30, metadata=metadata)
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(4, 4))
-    eixos = [1000 * x for x in [0, Nx * h, Ny * h, 0]]
-    im1 = ax1.imshow(u2[:, :, 0], extent=eixos, vmin=0, vmax=1)
-    im2 = ax1.imshow(meio, extent=eixos, alpha=0.1)
-    im3 = ax2.imshow(u4[:, :, 0], extent=eixos, vmin=0, vmax=1)
-    im4 = ax2.imshow(meio, extent=eixos, alpha=0.1)
+    # C = cmax*ts/h  # Condição CFL
 
-    #plt.colorbar()
-    #plt.xlabel('x [mm]')
-    #plt.ylabel('z [mm]')
+    Nx = meio.shape[0]
+    Ny = meio.shape[1]
+    Nt = 1000  # quantidade de instantes de tempo
 
-    with writer.saving(fig, f"C:/AUSPEX/videos/{nome}.mp4", 400):
-        for n in range(0, nit):
-            # Exibir
-            im1.set_data(u2[:, :, n])
-            ax1.set_title(f"u[{(n * ts):15.9f} s]")
+    # Posição transdutor
+    x_trans = 50
+    y_trans = 25
 
-            im3.set_data(u4[:, :, n])
-            ax2.set_title(f"u[{(n * ts):15.9f} s]")
+    # Inicialização dos vetores
+    u4 = np.zeros((Nx, Ny, Nt))  # u usando Laplaciano accuracy 4
+    V = np.zeros((Nx, Ny, Nt))  # Vij = ut ij 0 (derivada da condição inicial)
 
-            writer.grab_frame()
+    # Condições iniciais
+    V[:, :, :] = 0
+
+    if termo_fonte is None:
+        # Termo de fonte
+        t = np.arange(0, Nt, dtype=float)
+        t = t * ts
+        #f = np.zeros((1, Nt))
+        #f[0,0:50] = 1
+        f = signal.gausspulse(t-1.5e-6, 2.5e6, 0.5)
+        #plt.ioff()
+        #plt.figure()
+        #plt.plot(t, f)
+        #plt.show()
+        termo_fonte = np.zeros((Nx, Ny, Nt))
+        termo_fonte[y_trans, x_trans, :] = f
 
 
-# Dados da simulação
-ts = 10e-9  # discretização temporal (cada instante equivale a 10 ns)
-h = 200e-6  # discretização espacial (cada pixel equivale a 200 um)
-#cmax = 5800  # Velocidade máxima no meio (aço)
-#cmax = 1500  # Velocidade máxima no meio (água do mar)
+    # Calcula CFL para grid inteira
+    C2 = (meio[:, :] * ts / h) ** 2
 
-# C = cmax*ts/h  # Condição CFL
+    start_time = time.time()
 
-Nx = 100  # quantidade de pixels x
-Ny = 100  # quantidade de pixels y
-Nt = 500  # quantidade de instantes de tempo
+    # Com Laplaciano accuracy 4
+    # Primeiro passo
+    u4[:, :, 1] = u4[:, :, 0] + ts * V[:, :, 0] + 0.5 * C2 * laplaciano4(u4[:, :, 0])\
+        + 0.5 * ts ** 2 * termo_fonte[:, :, 0]
+
+    # Encontre cada ponto no array de posições para cada tempo
+    for n in range(1, Nt-1):
+        # Encontrar cada ponto no array de posições t[n+1]
+        u4[:, :, n + 1] = 2 * u4[:, :, n] - \
+                          u4[:, :, n - 1] + \
+                          C2 * lap4(u4[:, :, n]).reshape((Nx, Ny)) + \
+                          termo_fonte[:, :, n]
+
+        # Garantir condições nas bordas
+        u4[0, :, n + 1] = 0
+        u4[Nx - 1, :, n + 1] = 0
+        u4[:, 0, n + 1] = 0
+        u4[:, Ny - 1, n + 1] = 0
+
+    simulation_time2 = time.time()
+    tsim4 = simulation_time2 - start_time
+    print(f"Tempo para fazer a simulacao Laplaciano accuracy 4: {tsim4}")
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"Tempo total: {total_time}")
+
+    return u4
 
 # Matriz que representa o meio
+Nx = 100  # quantidade de pixels x
+Ny = 100  # quantidade de pixels y
+ts = 10e-9
+h = 200e-6
 meio = np.zeros((Nx, Ny))
-meio[0:36, 0:100] += 2500*np.ones((36, 100))  # Água do mar
-meio[36:100, 0:100] += 5800*np.ones((64, 100))  # Aço
+meio[0:36, 0:100] += 4000*np.ones((36, 100))
+meio[36:100, 0:100] += 5800*np.ones((64, 100))
+meio[60:80, 60:80] = 4000
+meio_true = np.copy(meio)
+u_true = simulacao(meio, h=h, ts=ts)
 
-# Posição transdutor
-x_trans = 50
-y_trans = 25
+meio = np.ones((Nx, Ny))*5000.
 
-# Inicialização dos vetores
-u2 = np.zeros((Nx, Ny, Nt))  # u usando Laplaciano accuracy 2
-V = np.zeros((Nx, Ny, Nt))  # Vij = ut ij 0 (derivada da condição inicial)
+maxit = 70
+dist = np.zeros(maxit, dtype=float)
+for n in range(100):
+    print('Simulation ' + str(n) + ' of ' + str(maxit))
+    u_guess = simulacao(meio, h=h, ts=ts)
+    u_res = u_guess - u_true
+    u_adj = simulacao(meio, termo_fonte=u_res, h=h, ts=ts)
+    grad = np.sum(u_adj * lap4(u_true)/(h**2), 2) * ts
+    meio = meio - grad/100
 
-# Condições iniciais
-V[:, :, :] = 0
+    difference = meio - meio_true
+    difference[25, 50] = 0
+    dist[n] = np.linalg.norm(difference)
+plt.figure()
+plt.subplot(121)
+plt.imshow(meio_true, vmin=2000, vmax=7000)
+plt.colorbar()
+plt.title('True model')
+plt.subplot(122)
+plt.imshow(meio, vmin=2000, vmax=7000)
+plt.colorbar()
+plt.title('Estimated model')
 
-# Termo de fonte
-t = np.arange(0, (Nt-1)*ts, ts)
-#f = np.zeros((1, Nt))
-#f[0,0:50] = 1
-f = signal.gausspulse(t, 5e6, 0.6)
-#plt.ioff()
-#plt.figure()
-#plt.plot(t, f)
-#plt.show()
-termo_fonte = np.zeros((Nx, Ny, Nt))
-termo_fonte[y_trans, x_trans, :] = f
-
-
-# Calcula CFL para grid inteira
-C2 = (meio[:, :] * ts / h) ** 2
-
-start_time = time.time()
-
-# Com Laplaciano accuracy 2
-# Primeiro passo
-u2[:, :, 1] = u2[:, :, 0] + ts * V[:, :, 0] + 0.5 * C2 * laplaciano2(u2[:, :, 0])\
-    + 0.5 * ts ** 2 * termo_fonte[:, :, 0]
-
-# Encontrar cada ponto no array de posições para cada tempo
-for n in range(1, Nt-1):
-    # Encontrar cada ponto no array de posições t[n+1]
-    u2[:, :, n + 1] = 2 * u2[:, :, n] - u2[:, :, n - 1] + C2 * laplaciano2(u2[:, :, n]) \
-        + termo_fonte[:, :, n]
-
-    # Garantir condições nas bordas
-    u2[0, :, n + 1] = 0
-    u2[Nx - 1, :, n + 1] = 0
-    u2[:, 0, n + 1] = 0
-    u2[:, Ny - 1, n + 1] = 0
-
-simulation_time1 = time.time()
-tsim2 = simulation_time1 - start_time
-print(f"Tempo para fazer a simulacao Laplaciano accuracy 2: {tsim2}")
-
-
-
-# nomes arquivos
-video_name = 'Pulso Gaussiano - Laplaciano 2 e 4'
-
-# Gera um vídeo a partir de cada plot
-gera_video_otimizacao(video_name, Nt)
-
-video_time = time.time()
-print(f"Tempo para fazer o video: {tvid}")
-
-end_time = time.time()
-total_time = end_time - start_time
-print(f"Tempo total: {total_time}")
+plt.figure()
+plt.plot(dist, 'o-')
+plt.xlabel('iteration')
+plt.ylabel(['error'])
+plt.grid()
